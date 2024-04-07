cdf70e934eea1ac37989d624f05de0b405a087f7
nikush0 <bochkovskiynikita@gmail.com>
2024-04-07 16:59:51 +0300

    home work

---
+++ src/js/2-form.js
@@ -0,0 +1,38 @@
+const nameKey = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const textForm = {
+  email: '',
+  message: '',
+};
+
+const getMessage = () => {
+  (textForm.email = form.elements.email.value.trim()),
+    (textForm.message = form.elements.message.value.trim()),
+    localStorage.setItem(nameKey, JSON.stringify(textForm));
+};
+
+const load = () => {
+  const data = localStorage.getItem(nameKey);
+  if (data) {
+    const { email, message } = JSON.parse(data);
+    form.elements.email.value = email;
+    form.elements.message.value = message;
+  }
+};
+
+load();
+
+function formSubmit(event) {
+  event.preventDefault();
+  if (form.elements.message.value !== '' && form.elements.email.value !== '') {
+    console.log(textForm);
+    form.reset();
+    localStorage.removeItem(nameKey);
+  } else {
+    alert('Не всі поля заповнені');
+  }
+}
+
+form.addEventListener('input', getMessage);
+
+form.addEventListener('submit', formSubmit);
